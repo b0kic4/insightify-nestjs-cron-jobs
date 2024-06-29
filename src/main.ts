609dc6a5d3bada0e5e98d9583631a81095efec78
Boris Nikolic <borisnikolic2302@gmail.com>
2024-06-29 15:35:56 +0200

    Fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(2000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      credentials: true,
+    },
+  });
+  app.use(express.json({ limit: '50mb' }));
+
+  const port = process.env.PORT || 5000;
+
+  // Use global validation pipe
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // Start the application
+  await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
